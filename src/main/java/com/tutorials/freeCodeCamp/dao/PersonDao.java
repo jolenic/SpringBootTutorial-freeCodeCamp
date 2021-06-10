e05eb37feb9bb5c6c3270735a43d924206fd944d
Jennifer <jennifer.olenic@gmail.com>
2021-06-10 17:26:20 -0400

    implemented all api request methods

--- src/main/java/com/tutorials/freeCodeCamp/dao/PersonDao.java
+++ src/main/java/com/tutorials/freeCodeCamp/dao/PersonDao.java
@@ -2,13 +2,23 @@ package com.tutorials.freeCodeCamp.dao;
 
 import com.tutorials.freeCodeCamp.model.Person;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface PersonDao {
     int insertPerson(UUID id, Person person);
 
-    default int insertPerson(Person person){
+    default int insertPerson(Person person) {
         UUID id = UUID.randomUUID();
         return insertPerson(id, person);
     }
+
+    List<Person> selectAllPeople();
+
+    int deletePersonById(UUID id);
+
+    int updatePersonById(UUID id, Person person);
+
+    Optional<Person> selectPersonById(UUID id);
 }
